2a62e3ab1c1b127eebb9057fb201300748bd9ffb
Bugg6 <jexshadow@gmail.com>
2025-02-07 10:56:51 -0500

    adding game window

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from constants import *
+
+
+def main():
+    pygame.init()
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        pygame.Surface.fill(window, color=(0, 0, 0))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+if __name__ == "__main__":
+    main()
